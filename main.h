ba69f2e62c08b4775f0efe191f165f1188cd5101
DeshonS <deshonjewell@gmail.com>
2024-07-29 14:40:20 -0500

    added more prototypes

--- main.h
+++ main.h
@@ -13,4 +13,9 @@ int new_exit(char **request);
 int new_cmd(char **request);
 void shell_ni_loop(void);
 
+int new_cd(char **request);
+int new_env(char **request);
+int new_exit(char **request);
+int new_help(char **request);
+int new_cwd(char **request);
 #endif
