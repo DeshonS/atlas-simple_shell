2ae5c9695d72e854fb584e1bf099139d8d9ce226
DeshonS <deshonjewell@gmail.com>
2024-07-29 14:47:13 -0500

    Getting undefined error, troubleshooting

--- main.h
+++ main.h
@@ -12,10 +12,10 @@ int new_env(char **request);
 int new_exit(char **request);
 int new_cmd(char **request);
 void shell_ni_loop(void);
-
 int new_cd(char **request);
 int new_env(char **request);
 int new_exit(char **request);
 int new_help(char **request);
 int new_cwd(char **request);
+
 #endif
