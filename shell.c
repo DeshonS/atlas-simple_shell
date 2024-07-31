30e6a66bf60141d9088bba7b9bb19e6411f421d8
DeshonS <deshonjewell@gmail.com>
2024-07-31 17:18:57 -0500

    at this point im trying anyhthing

--- shell.c
+++ shell.c
@@ -1,95 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <string.h>
 
-int run_cmd(char *cmd, char *flags[])
-{
-    pid_t pid = fork();
+#define MAX_CMD_LEN 100
 
-    if (pid == -1)
-    {
-        perror("fork");
-        return 1;
-    }
-    else if (pid == 0)
-    {
-        char *args[10];
-        char *env[] = {"PATH=/bin", NULL};
-        int i = 0;
-
-        if (strcmp(cmd, "ls") == 0 || strcmp(cmd, "pwd") == 0 || strcmp(cmd, "date") == 0)
-        {
-            args[i++] = cmd;
-            while (flags && *flags)
-            {
-                args[i++] = *flags++;
-            }
-            args[i] = NULL;
-        }
-        else
-        {
-            fprintf(stderr, "Unknown command: %s\n", cmd);
-            exit(EXIT_FAILURE);
-        }
-
-        if (execve(args[0], args, env) == -1)
-        {
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        int status;
-        waitpid(pid, &status, 0);
-
-        if (WIFEXITED(status))
-        {
-            printf("Child exited with status %d\n", WEXITSTATUS(status));
-        }
-        else
-        {
-            printf("Child did not exit successfully\n");
-        }
-    }
-
-    return 0;
+void prompt() {
+    printf("simple_shell> ");
 }
 
-int main()
-{
-    char *cmd = NULL;
-    char *input = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    char *flags[10];
-    int i = 0;
-    char *token = strtok(input, " ");
+int main() {
+    char cmd[MAX_CMD_LEN];
+    char *args[2];
+    int status;
+    
+    while (1) {
+        prompt();
 
-    printf("Enter a command and flags: ");
-    nread = getline(&input, &len, stdin);
-    if (nread == -1)
-    {
-        perror("getline");
-        free(input);
-        return 1;
+        if (fgets(cmd, MAX_CMD_LEN, stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+
+        cmd[strcspn(cmd, "\n")] = '\0';  // Remove the newline character
+
+        if (strlen(cmd) == 0) {
+            continue;
+        }
+
+        args[0] = cmd;
+        args[1] = NULL;
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("Fork failed");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            if (execve(cmd, args, environ) == -1) {
+                perror("Error");
+            }
+            exit(EXIT_FAILURE);
+        } else {
+            wait(&status);
+        }
     }
-
-    input[strcspn(input, "\n")] = 0;
-
-    cmd = strdup(token);
-
-    while ((token = strtok(NULL, " ")) != NULL)
-    {
-        flags[i++] = token;
-    }
-    flags[i] = NULL;
-
-    run_cmd(cmd, flags);
-
-    free(cmd);
-    free(input);
     return 0;
 }
