ccdde50a31e82384dcd9cb1fcf77d698bd638286
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2025-02-12 20:44:53 -0600

    Update run_cmd.c

--- run_cmd.c
+++ run_cmd.c
@@ -8,10 +8,15 @@ typedef struct command {
 void run_cmd(char **tokens)
 {
     pid_t pid = fork();
-    int i;
+    int i, fd = -1;
+    char *args[100];
+    char *filename = NULL;
+    int j = 0;
+
     command commands[] = {
         {"ls", "/bin/ls"},
         {"exit", "/bin/exit"},
+        {NULL, NULL} 
     };
 
     if (pid == -1)
@@ -23,12 +28,47 @@ void run_cmd(char **tokens)
     {
         char *env[] = {"PATH=/bin", NULL};
 
-        printf("%s\n", tokens[0]);
+        for (i = 0; tokens[i] != NULL; i++)
+        {
+            if (strcmp(tokens[i], ">") == 0)
+            {
+                if (tokens[i + 1] != NULL)
+                {
+                    filename = tokens[i + 1];
+                    break;
+                }
+                else
+                {
+                    fprintf(stderr, "Syntax error: Missing filename after '>'\n");
+                    exit(1);
+                }
+            }
+            args[j++] = tokens[i];
+        }
+        args[j] = NULL;
+
+        if (filename)
+        {
+            fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd == -1)
+            {
+                perror("open");
+                exit(1);
+            }
+            if (dup2(fd, STDOUT_FILENO) == -1)
+            {
+                perror("dup2");
+                close(fd);
+                exit(1);
+            }
+            close(fd);
+        }
+
         for (i = 0; commands[i].input != NULL; i++)
         {
-            if (strcmp(commands[i].input, tokens[0]) == 0)
+            if (strcmp(commands[i].input, args[0]) == 0)
             {
-                execve(commands[i].path, tokens, env);
+                execve(commands[i].path, args, env);
                 perror("execve error");
                 exit(EXIT_FAILURE);
             }
